8a412084961ab617337c52cff17ac0a45b841012
Jojo3235 <joralmand@gmail.com>
2023-04-28 01:39:52 +0200

    cambio menor

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,13 +1,3 @@
-# Desarrollar un algoritmo que permita cargar 1000 número enteros –generados de manera alea- toria– que resuelva las siguientes actividades:
-
- 
-
-# realizar los barridos preorden, inorden, postorden y por nivel sobre el árbol generado;
-#  determinar si un número está cargado en el árbol o no;
-#  eliminar tres valores del árbol;
-#  determinar la altura del subárbol izquierdo y del subárbol derecho;
-#  determinar la cantidad de ocurrencias de un elemento en el árbol;
-
 import random
 
 class nodoCola(object):
